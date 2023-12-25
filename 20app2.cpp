71f6c02e5c0a706bdad2e5e102bd97d0c1d2e8f8
root <root@localhost.localdomain>
2023-12-25 14:47:21 +0000

    20 lesson with apply

---
+++ 20app2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  cout << "==============================\n";
+  cout << "==Convert To ASCII Character==\n";
+  cout << "==============================\n";
+
+  cout << "=======Enter a Character======\n";
+
+  int a;
+  cin >> a;
+
+  cout << char('a') << "\n";
+
+  return 0;
+}
